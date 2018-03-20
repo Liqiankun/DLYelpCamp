fbe89897dcd12f4ca6090640d9deb2caecbadcda
David <qiankunli@yahoo.com>
2018-03-20 10:07:22 +0800

    restyle login and register page

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ router.post('/register', function (req, res) {
   var newUser = new User({ username: req.body.username })
   User.register(newUser, req.body.password, function (err, user) {
     if (err) {
-      console.log(err)
+      console.log('err', err.message)
+      req.flash('error', err.message)
       return res.render('register')
     } else {
       passport.authenticate('local')(req, res, function () {
