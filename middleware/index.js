64593d3e54ee7faa61ea8dfc0c0591edf2dc4171
David <qiankunli@yahoo.com>
2018-03-20 10:00:44 +0800

    handle err and success

--- middleware/index.js
+++ middleware/index.js
@@ -26,16 +26,19 @@ function checkCampgroundOwnership (req, res, next) {
   if (req.isAuthenticated()) {
     Campground.findById(req.params.id, function (err, campground) {
       if (err) {
+        req.flash('error', 'Campground not found')
         res.redirect('back')
       } else {
         if (campground.author.id.equals(req.user._id)) {
           next()
         } else {
+          req.flash('error', 'You don not permission to do that')
           res.redirect('back')
         }
       }
     })
   } else {
+    req.flash('error', 'You have to log in first')
     res.redirect('/login')
   }
 }
@@ -45,7 +48,7 @@ function isLoggedIn (req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   } else {
-    req.flash('error', 'You need to log in first')
+    req.flash('error', 'You need to log first!')
     res.redirect('/login')
   }
 }
